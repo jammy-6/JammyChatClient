9fb2113508eec3d2971f26f7647eb503b842d977
Jammy <920817290@qq.com>
2024-11-06 00:09:00 +0800

    1.新增注册成功提示窗口 2.新增重置密码窗口

---
+++ include/RegisterSuccessHintWindow.h
@@ -0,0 +1,63 @@
+#ifndef REGISTERSUCCESSHINTWINDOW_H
+#define REGISTERSUCCESSHINTWINDOW_H
+
+#include <QWidget>
+#include <QMenu>
+#include <QAction>
+#include <QList>
+#include <QLabel>
+#include <QContextMenuEvent>
+#include <QNetworkAccessManager>
+#include <QMessageBox>
+#include <QNetworkReply>
+#include <QDate>
+#include <QHttpMultiPart>
+#include <QPainter>
+#include <QPen>
+#include <QChart>
+#include <QLineSeries>
+#include <QChartView>
+#include <QValueAxis>
+#include <QScrollArea>
+#include <algorithm>
+#include <QTimer>
+#include <QMutex>
+#include <functional>
+#include <QColor>
+#include <QFrame>
+#include <QApplication>
+#include <QLayout>
+#include <QPushbutton>
+#include <qlineedit.h>
+#include <QTimer>
+#include "MoveableWidget.h"
+class RegisterSuccessHintWindow : public MoveableWidget
+{
+    Q_OBJECT
+
+public:
+    explicit RegisterSuccessHintWindow(QWidget* parent = nullptr);
+    void initUI();
+    void initConnect();
+    ~RegisterSuccessHintWindow();
+
+
+protected:
+    void paintEvent(QPaintEvent* event) override;
+    void showEvent(QShowEvent* event) override;
+    void hideEvent(QHideEvent* event) override;
+signals:
+    void signalReturnLoginWindow();
+private:
+
+    QLabel* titleLabel;
+    QLabel* hintLabel;
+    QPushButton* returnBtn;
+    QTimer *timer_;
+private slots:
+    
+
+
+};
+
+#endif // !1
---
+++ include/ResetPasswordWindow.h
@@ -0,0 +1,83 @@
+#ifndef RESETPASSWORDWINDOW_H
+#define RESETPASSWORDWINDOW_H
+
+#include <QWidget>
+#include <QMenu>
+#include <QAction>
+#include <QList>
+#include <QLabel>
+#include <QContextMenuEvent>
+#include <QNetworkAccessManager>
+#include <QMessageBox>
+#include <QNetworkReply>
+#include <QDate>
+#include <QHttpMultiPart>
+#include <QPainter>
+#include <QPen>
+#include <QChart>
+#include <QLineSeries>
+#include <QChartView>
+#include <QValueAxis>
+#include <QScrollArea>
+#include <algorithm>
+#include <QTimer>
+#include <QMutex>
+#include <functional>
+#include <QColor>
+#include <QFrame>
+#include <QApplication>
+#include <QLayout>
+#include <QLineEdit>
+#include "CustomerWidget.h"
+#include "Global.h"
+#include "CustomTimerBtn.h"
+#include "MoveableWidget.h"
+class Controller;
+class ResetPasswordWindow : public MoveableWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ResetPasswordWindow(QWidget *parent = nullptr);
+    void initUI();
+    void initConnect();
+    void updateMsgHint(QWidget* widget, QString hint, bool status);
+    ///用于判断用户所填入的信息是否有误
+    bool judgeInfo();
+    bool judgeResetInfoComplete();
+    ///判断当前所填入信息是否可以发送验证码
+    bool judgeResetInfoCanSendCode();
+    ~ResetPasswordWindow();
+
+
+protected:
+    void paintEvent(QPaintEvent* event);
+    void contextMenuEvent(QContextMenuEvent* event) override;
+
+private:
+    friend class Controller;
+
+    QMenu contextMenu;
+    QAction* reloadQssAction;
+    QLabel* titleLabel;
+    QLabel* msgHintLabel;
+
+    CustomWidgetLabel_LineEdit *resetUser;
+    CustomWidgetLabel_LineEdit * resetEmail;
+
+    QLabel* resetValidateCodeLabel;
+    QLineEdit* resetValidateCodeEdit;
+    CustomTimerBtn* getValidateCodeBtn;
+
+    CustomWidgetLabel_LineEdit * resetPassword;
+    QPushButton* resetPasswordVisible;
+
+    QPushButton* confirmBtn;
+    QPushButton* returnBtn;
+private slots:
+    void reloadQss();
+    
+
+};
+
+#endif 
