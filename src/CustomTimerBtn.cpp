6a0c1a6b999b0c8b33c97486a53669812ca96150
Jammy <920817290@qq.com>
2024-11-06 00:15:10 +0800

    1.新增倒计时按钮

---
+++ src/CustomTimerBtn.cpp
@@ -0,0 +1,33 @@
+#pragma execution_character_set("utf-8")
+#include "CustomTimerBtn.h"
+
+CustomTimerBtn::CustomTimerBtn(QWidget* parent) {
+	timer_ = new QTimer(this);
+	connect(timer_, &QTimer::timeout, this, &CustomTimerBtn::slotTimeout);
+}
+CustomTimerBtn::~CustomTimerBtn() {
+	timer_->stop();
+}
+void CustomTimerBtn::slotTimeout() {
+	if (countdown_ <= 0) {
+		countdown_ = 10;
+		this->setEnabled(true);
+		this->setText(QString("»ñÈ¡"));
+		timer_->stop();
+		return;
+	}
+	countdown_--;
+	this->setText(QString::number(countdown_));
+	timer_->start();
+	
+}
+
+void CustomTimerBtn::mouseReleaseEvent(QMouseEvent* e) {
+	if (e->button() == Qt::LeftButton) {
+		this->setText(QString::number(countdown_));
+		timer_->start(1000);
+		this->setEnabled(false);
+		emit clicked();
+	}
+	QPushButton::mouseReleaseEvent(e);
+}
